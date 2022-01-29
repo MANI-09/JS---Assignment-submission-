afe87ae3158b53b6058e01e6864e7b874f8fbc35
MANI-09 <98636058+MANI-09@users.noreply.github.com>
2022-01-30 00:27:07 +0530

    day 3 assignment submitted
    user input email id and password

---
+++ day 3/app.js
@@ -0,0 +1,12 @@
+
+    
+    const email = prompt("Enter your email id ??");
+    const password = prompt(" Enter your password ??");
+
+if (email === "stu@letsupgrade.com" && password ==="12345")
+{
+    alert("welcome sir!");11
+}
+ else {
+    alert("invalid email id or password!!TRY AGAIN!");
+}
